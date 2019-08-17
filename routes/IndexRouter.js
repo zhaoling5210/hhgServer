8d674bcaaa6cc46893e97f16c00562ce72c6b8d3
zl <279931916@qq.com>
2019-08-17 21:46:37 +0800

    IndexRouter.js

--- routes/IndexRouter.js
+++ routes/IndexRouter.js
@@ -6,9 +6,30 @@ Router.get("/floor", (req, res) => {
   let sql = `SELECT img_url ,img_type ,fid FROM wedpic_index`;
   pool.query(sql, [], (err, result) => {
     if (result.length) {
+      let r = [];
+      let enumType = ["big", "small1", "small2"];
+      // 处理结果
+      result.forEach((item, index, arr) => {
+        if (!Array.isArray(r[item.fid])) {
+          r[item.fid] = [];
+        }
+        r[item.fid].push({
+          img_url: item.img_url,
+          img_type: enumType[item.img_type]
+        });
+      });
+      // 删除空数组/空项目
+      for (let i = r.length - 1; i >= 0; i--) {
+        if (Array.isArray(r[i])) {
+          if (!(r.length>0)) {
+            r.splice(i, 1);
+          }
+        } else {
+          r.splice(i, 1);
+        }
+      }
       // 使用JSONP 跨域 返回用 callback('') 包裹着的 JSON 字符串
-      console.log(result);
-      res.send(`${req.query.callback}(${JSON.stringify(result)})`);
+      res.send(`${req.query.callback}(${JSON.stringify(r)})`);
     }
   });
 });
