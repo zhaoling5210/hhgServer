70e35ba1e826d9d566a9de072bf2eef275ebedef
zl <279931916@qq.com>
2019-09-15 16:53:50 +0800

    finish

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require("express");
 // const pool = require("./pool");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const IndexRouter = require("./routes/IndexRouter");
 
-const serve = express();
-serve.listen(9999);
+const server = express();
+server.listen(9999);
 
-serve.use(
+server.use(express.static("image"));
+
+server.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
 
-serve.use(IndexRouter);
+// 配置跨域
+server.use(
+  cors({
+    origin: ['http://localhost:8080','http://localhost:8081'],
+    credentials: true
+  })
+);
+
+server.use(IndexRouter);
